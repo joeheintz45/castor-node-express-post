8d6c74fb0786c613426b25992869664bd3978273
Myron <dubtwee@gmail.com>
2020-10-14 16:24:21 -0500

    adding code samples for testing get routes on postman

--- server/server.js
+++ server/server.js
@@ -3,6 +3,33 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const peopleList = [
+  {
+    name: 'Scott',
+    age: 37,
+  },
+  {
+    name: 'Myron',
+    age: 37,
+  },
+  {
+    name: 'Rachael',
+    age: 35,
+  },
+];
+
+app.get('/people/last', (req, res) => {
+  res.send(peopleList[peopleList.length - 1]);
+});
+
+app.get('/people/first', (req, res) => {
+  res.send(peopleList[0]);
+});
+
+app.get('/people', (req, res) => {
+  res.send(peopleList);
+});
+
 app.get('/currency', (req, res) => {
   res.send('Hello Money');
 });
